f877a55e70108c4ee8a60d467dc169b94971c759
Nayara Ribeiro Freire <nayfreireof@gmail.com>
2021-03-03 00:30:34 -0300

    Colocadas as bombas nos slots

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
 // $(document).ready(function(){
 //     //Click normal
-//     $('.slot').click(function(){
+//      $('.slot').click(function(){
 //         alert('clicado')
-//     })
+//      })
 
 //     //Click para a bandeira
-//     $('.slot').contextmenu(function(){
+//      $('.slot').contextmenu(function(){
 //         alert('bandeira')
-//     })
+//      })
+
+//      //Clica para mostrar o id do slot
+//      $('.slot').click(function(){
+//         $(this).append(this.id)
+//      })
 // })
 
 $(document).ready(function(){
     
-    var colNum = 10;
-    var rowNum = 10;
+    var colNum = 9;
+    var rowNum = 9;
     var bombNum = (colNum * rowNum)/10;
     var flagCounter = bombNum;
     var counter = 1;
     var bombPositions = new Array();
 
-    for(var i=0; i<colNum; i++){
-        $('#matriz').append("<div class='linhas' >")
-        for(var j=0; j<rowNum; j++){
-<<<<<<< HEAD
-            $('.linhas').append('<div id="'+counter+'" class="slot tile_'+counter+'"></div>');
-            counter++;
-=======
-          $('.linhas').append('<div id="'+counter+'" class="slot tile_'+counter+'"></div>');
-          counter++;
-          
->>>>>>> 6dd75df9684d50073b755d7aab804bb661a48df5
-        }
-        j=0;
-        $('#matriz').append("</div>")
-    }
-
+    // for(var i=0; i<colNum; i++){
+    //     $('#matriz').append("<div class='linhas' >")
+    //     for(var j=0; j<rowNum; j++){
+    //         $('.linhas').append('<div id="'+counter+'" class="slot tile_'+counter+'"></div>');
+    //         counter++;
+    //     }
+    //     j=0;
+    //     $('#matriz').append("</div>")
+    // }
 
     CriarBombas();
 
@@ -60,20 +58,71 @@ $(document).ready(function(){
             var posY = parseInt(Math.random()*rowNum);
             coordenada.push(posX);
             coordenada.push(posY)
+            $('#'+posX+'-'+posY).addClass('bomba')
 
-            bombPositions.push(coordenada)            
-            coordenada = new Array();
+            let naoRepete = VerificaTodos(bombPositions,coordenada)
+
+            if(naoRepete){
+                bombPositions.push(coordenada)            
+                coordenada = new Array();
+            }
+            else{
+                coordenada = new Array();
+            }
         }
         console.log({bombPositions})
-    }
+        VerificaSeTemDezBombas(bombPositions)
 
+        function VerificaSeTemDezBombas(bombPositions){
+            if(bombPositions.length == 10){
+                return true
+            }
+            else{
+                let quantFaltaPara10 = 10 - bombPositions.length
 
-    
-
-    // let i
-    // for(i=0;i<81;i++){
+                for(i=0;i<=quantFaltaPara10;i++){
+                    var posX = parseInt(Math.random()*colNum);
+                    var posY = parseInt(Math.random()*rowNum);
+                    coordenada.push(posX);
+                    coordenada.push(posY)
+                    $('#'+posX+'-'+posY).addClass('bomba')
         
-    // }
+                    let verifica = VerificaTodos(bombPositions, coordenada)
+        
+                    if(verifica){
+                        bombPositions.push(coordenada)            
+                        coordenada = new Array();
+                    }
+                }
+            }
+        }
+
+        function VerificaSeRepete(coordenada){
+            var count = bombPositions.length
+
+            if(JSON.stringify(coordenada)==JSON.stringify(bombPositions[count-1])){
+                // coordenada = new Array()
+                return false
+            }
+
+            return true
+        }
+
+        function VerificaTodos(bombPositions, coordenada){
+            let igual = 0
+            for(i=0;i<bombPositions.length;i++){
+                if(JSON.stringify(coordenada)==JSON.stringify(bombPositions[i])){
+                    igual++;
+                }
+            }
+
+            if(igual > 0){
+                return false
+            }
+
+            return true
+        }
+    }
 
 }
 );
